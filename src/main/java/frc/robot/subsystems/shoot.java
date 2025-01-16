f71b97369ca868b1db5fcf5b9440645df4a95e78
thorkiller35 <59945459+thorkiller35@users.noreply.github.com>
2025-01-15 19:51:49 -0500

    We love Kracken Swerve

---
+++ src/main/java/frc/robot/subsystems/shoot.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class shoot extends SubsystemBase{
+    private final TalonFX shootMotor = new TalonFX(21);
+
+    public shoot(){
+
+    }
+    @Override
+    public void periodic(){
+        // This method will be called once per scheduler run
+    }
+
+    public Command shootcmd(double speed){
+    
+        
+    return new Command() {
+        @Override
+        public void initialize() {
+            // Initialization code, such as resetting encoders or PID controllers
+
+        }
+
+        @Override
+        public void execute() {
+            shootMotor.set(speed);
+          
+        }
+
+        @Override
+        public void end(boolean interrupted) {
+        
+          
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+    };
+  }
+   
+    
+}
